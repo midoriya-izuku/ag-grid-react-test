66f6418b643de98c52baaec0e697972e22cb1d7a
midoriya-izuku <sairajmayekar12@gmail.com>
2021-01-15 11:48:35 +0530

    update button style

--- src/containers/TableActionContainer.js
+++ src/containers/TableActionContainer.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { useGrid } from "../contexts/GridContext";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  button: {
+    margin: "10px",
+  },
+}));
 const TableActionContainer = () => {
   let { submitGrid, addNewRow, removeSelected, removeNonSelected } = useGrid();
-
+  const classes = useStyles()
   return (
     <div>
-      <Button onClick={addNewRow}>Add Row</Button>
-      <Button onClick={removeSelected}>Delete Selected Rows</Button>
-      <Button onClick={removeNonSelected}>Delete Non Selected Rows</Button>
-      <Button onClick={submitGrid}>Submit Data</Button>
+      <Button className={classes.button} variant="contained" color="primary" onClick={addNewRow}>Add Row</Button>
+      <Button className={classes.button} variant="contained" color="primary" onClick={removeSelected}>Delete Selected Rows</Button>
+      <Button className={classes.button} variant="contained" color="primary" onClick={removeNonSelected}>Delete Non Selected Rows</Button>
+      <Button className={classes.button} variant="contained" color="primary" onClick={submitGrid}>Submit Data</Button>
     </div>
   );
 };
--- src/renderers/DeleteButtonComponent.js
+++ src/renderers/DeleteButtonComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { useGrid } from "./../contexts/GridContext";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(() => ({
+  button: {
+    height:"20px"
+  },
+}));
 const DeleteButtonComponent = ({ rowIndex }) => {
   let { removeRow } = useGrid();
-  return <button onClick={() => removeRow(rowIndex)}>Delete</button>;
+  let classes = useStyles()
+  return <Button className={classes.button} variant="contained" color="secondary" onClick={() => removeRow(rowIndex)}>Delete</Button>;
 };
 
 export default DeleteButtonComponent;
